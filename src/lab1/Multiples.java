79d3028d0d42ea1e7421db4eb216452a66e534fd
Roger He <rogerhe0829@Rogers-MacBook-Pro-4.local>
2025-09-12 15:04:34 -0400

    9/12/2025 update 1

--- src/lab1/Multiples.java
+++ src/lab1/Multiples.java
@@ -1,12 +1,19 @@
 package lab1;
 
 public class Multiples {
-    public static void main(String[] args) {
+
+    public static int multiples(int n, int a, int b) {
         int acc = 0;
-        for (int i = 1; i < 1000; i++) {
-            if (i % 3 == 0 || i % 5 == 0) {
+        for (int i = 1; i < n; i++) {
+            if (i % a == 0 || i % b == 0) {
                 acc += 1;
             }
-        } System.out.println(acc);
+        }
+        return acc;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(multiples(10, 2, 3));
     }
 }
+
